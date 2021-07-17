364721200d002162a1f9f8775662b1a6008cffb1
Aghaz1-alphonic <aghaz.junaid@alphonic.in>
2021-07-18 00:39:18 +0530

    prod page setup

--- src/containers/Products/index.js
+++ src/containers/Products/index.js
@@ -1,12 +1,43 @@
-import React from 'react'
-import Layout from '../../components/Layout';
+import React, { useEffect, useState } from "react";
+import Layout from "../../components/Layout";
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  Table,
+  Modal,
+  Form,
+} from "react-bootstrap";
+import { useSelector, useDispatch } from "react-redux";
+import { getAllCategory, addCategory, deleteCategories } from "../../actions";
+import { useHistory } from "react-router-dom";
 
 function Products() {
-    return (
-        <Layout sidebar>
-            <h3>Products</h3>
-        </Layout>
-        )
+  return (
+    <Layout sidebar>
+      <Container>
+        <Row>
+          <Col md={12}>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                marginTop: "5px",
+                marginBottom: "20px",
+              }}
+            >
+              <h3>Product</h3>
+              <Button variant="danger">
+                Add
+              </Button>
+            </div>
+          </Col>
+        </Row>
+
+      </Container>
+    </Layout>
+  );
 }
 
-export default Products
+export default Products;
