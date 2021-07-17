63176c1c9b1f1d3405443d3824a4920770a9dc32
Aghaz1-alphonic <aghaz.junaid@alphonic.in>
2021-07-17 23:09:30 +0530

    adding modal

--- src/actions/category.actions.js
+++ src/actions/category.actions.js
@@ -22,7 +22,33 @@ export const getAllCategory = () => {
                 payload: { error: res.data.error }
             });
         }
+    }
+}
 
+export const addCategory = (catData) => {
+    console.log("catData", catData)
 
+    return async dispatch => {
+        
+        dispatch({ type: categoryConstansts.ADD_NEW_CATEGORY_REQUEST });
+        const res = await axios.post(`category`, {
+            ...catData
+        });
+        debugger
+        console.log(res);
+        if (res.status === 200) {
+
+            const addedCategory = res.data;
+
+            dispatch({  
+                type: categoryConstansts.ADD_NEW_CATEGORY_SUCCESS,
+                payload:  addedCategory 
+            });
+        } else {
+            dispatch({
+                type: categoryConstansts.ADD_NEW_CATEGORY_FAILURE,
+                payload: { error: res.data.error }
+            });
+        }
     }
 }
